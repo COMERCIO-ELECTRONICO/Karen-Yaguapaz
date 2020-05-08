7bb9f0c7be2ce35a8dc4c8078e1163b74a0bb32c
Karen Yaguapaz <karen.piano4@gmail.com>
2020-05-07 19:32:57 -0500

    Estructura
    Funciones | Arreglos

--- 01-javascript.js
+++ 01-javascript.js
@@ -6,6 +6,7 @@ var json = {
     fecha: new Date('1996/03/13'),
 }
 
+
 console.log(json.nombre)
 console.log(json['edad'])
 
@@ -22,3 +23,48 @@ console.log(json)
 /*console.error()// da eror
 console.info()
 console.warn() //da alerta*/
+
+
+
+var json2 = {
+    nombre: 'Karen',
+    edad: 25,
+    soltero: false,
+    fecha: new Date('1996/03/13'),
+    intereses: {
+        hobbies: [
+            'música',
+            'películas'
+        ],
+        peliculas: [{
+            nombre: '',
+            tipo: '',
+            esBuena: true,
+        },
+        {
+            nombre: '',
+            tipo: '',
+            esBuena: true,
+        }
+        ]
+    },
+    
+    familia:[
+        {
+            nombre: '',
+            edad: ''
+        }
+    ]
+
+}
+
+//metodo stringify a nuestro objeto le hace string
+var jsoneString = JSON.stringify(json2)
+
+console.log(json2)
+console.log(jsoneString)
+
+
+// Parse lo vuelve a hacer objeto
+var jsonParse = JSON.parse(jsoneString)
+console.log(jsonParse)
