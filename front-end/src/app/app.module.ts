7e8dabde6c13e841cda3af3e1611ee50453a4ee9
Karen Yaguapaz <karen.piano4@gmail.com>
2020-05-15 15:24:58 -0500

    Nuevo Proyecto

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
+import { PadreComponent } from './padre/padre.component';
+import { HijoComponent } from './hijo/hijo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiPrimerComponenteComponent
+    MiPrimerComponenteComponent,
+    PadreComponent,
+    HijoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule //modulo del formulario
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/mi-primer-componente/mi-primer-componente.component.ts
+++ front-end/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
 //@nombreDecorador() -> es un decorador, es una clase especial de declaración que puede acoplarse a una clase, método, propiedad o parámetro
 @Component({
   selector: 'app-mi-primer-componente',
   templateUrl: './mi-primer-componente.component.html',
   styleUrls: ['./mi-primer-componente.component.css']
 })
-export class MiPrimerComponenteComponent implements OnInit {
+export class MiPrimerComponenteComponent implements OnInit, OnDestroy {
 
   @Input() titulo: string;
   @Input() nombreBoton: string;
@@ -14,10 +14,20 @@ export class MiPrimerComponenteComponent implements OnInit {
 
   @Output() saludoHijo = new EventEmitter();
 
+  fecha = new Date();
+  sueldo: number= 3.1416;
+  nombre: string = 'Karen';
 
   constructor() { }
 
   ngOnInit() {
+    console.log
+    ('datos del padre en hijo')
+    console.log(this.datos)
+  }
+
+  ngOnDestroy(){
+
   }
 
   SALUDAR() {
