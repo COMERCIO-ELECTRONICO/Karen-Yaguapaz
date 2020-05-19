bb28d7215edad4ec0c330f60083b76cb6d51ea9a
Karen Yaguapaz <karen.piano4@gmail.com>
2020-05-19 12:10:48 -0500

    Rutas
    Nuevo Proyecto: Rutas | Módulos | Componentes

---
+++ rutas-angular/src/app/app.routes.ts
@@ -0,0 +1,42 @@
+import { NgModule} from "@angular/core";
+import {Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from './inicio/home/home.component';
+import { NoEncontradoComponent } from './inicio/no-encontrado/no-encontrado.component';
+import { LoginComponent } from './login/login.component';
+
+const rutas: Routes =[
+    {
+        path: 'inicio',
+        component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'usuario',
+        loadChildren: ()=>import('./usuario/usuario.module')
+        .then(usuario=>usuario.UsuarioModule)
+    },
+    {
+        path: '',
+        redirectTo:'inicio',
+        pathMatch:'full' //significa que va a tener toda la ruta
+    },
+    {
+        path: '**',
+        component: NoEncontradoComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(rutas)],
+    providers: [],
+    declarations:[],
+    exports: [RouterModule]
+})
+
+//con la palabra export podemos llamar el comenponen en cualquier lado
+export class AppRoutesComponent{
+
+}
